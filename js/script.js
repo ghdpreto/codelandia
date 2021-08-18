a0defc7ab6ba1f968276920cf658616554211ce2
ghdpreto <ghdpreto@gmail.com>
2021-08-18 15:04:43 -0300

    pega dados do localstorage

--- js/script.js
+++ js/script.js
@@ -11,3 +11,90 @@ btnCloseModal.addEventListener('click', () => {
 })
 
 
+
+const formModal = document.querySelector('#form')
+const btn = document.querySelector('[data-js = btnSave]')
+const item = []
+
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const title = document.querySelector("#title")
+    const date = document.querySelector('#date')
+    const description = document.querySelector('#description')
+    
+
+    let value = {
+        title: title.value,
+        date: date.value,
+        description: description.value,
+        like: false
+    }
+
+    
+    const local = localStorage.getItem('codelandia')
+    
+    console.log(local)
+    item.push(value)
+
+    // VERIFICAR
+    if(!localStorage) {
+        localStorage.setItem('codelandia',JSON.stringify(item))
+    } else {
+        localStorage.setItem('codelandia', JSON.stringify(item))        
+    }
+
+})
+
+
+function getLocalStorage() {
+    const value = JSON.parse(localStorage.getItem('codelandia'))
+    if(!value ) {
+        console.log('nada consta')
+    } else {
+
+        value.forEach(item => {
+            
+            const dateSecond = Date.parse(item.date)
+            
+            
+            //formatando a data
+            const dateFormater = new Date(dateSecond).toLocaleDateString('pt-br', {
+                year: 'numeric',
+                month: 'short',
+                day: '2-digit'
+            })
+            
+            //fazendo uma copia do array
+            let array = item
+        //criando um novo array com a data formatada
+        array.date = dateFormater
+        console.log(array)
+
+        //exibindo as informações no html
+        const container = document.querySelector('[data-js=container]')
+        const html = `
+        <section class="c-card">
+        <div class="c-card__header">
+          <p class="c-card__date">${array.date}</p>
+          <div class="c-card__icon">
+          <img src="./assets/heart-outlined.svg" alt="Curtir" />
+          </div>
+          </div>
+          
+          <div>
+          <h1 class="c-card__title">${array.title}</h1>
+          <p class="c-card__description">
+          ${array.description}
+          </p>
+          </div>
+          </section>
+          `
+          container.innerHTML += html
+        }
+        );
+    }
+}
+
+
+getLocalStorage()
