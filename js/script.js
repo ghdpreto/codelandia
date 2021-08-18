23fecb554bfd8201e0d5e20d0638e0873ccd1902
ghdpreto <ghdpreto@gmail.com>
2021-08-18 20:20:34 -0300

    ajuste na logica retorno sem coteudo

--- js/script.js
+++ js/script.js
@@ -1,84 +1,79 @@
-const btnOpenModal = document.querySelector('[data-js = btnOpenModal]')
-const btnCloseModal = document.querySelector('[data-js = btnCloseModal') 
-const modal = document.querySelector('[data-js = modal]')
+const btnOpenModal = document.querySelector("[data-js = btnOpenModal]");
+const btnCloseModal = document.querySelector("[data-js = btnCloseModal");
+const modal = document.querySelector("[data-js = modal]");
 
-btnOpenModal.addEventListener('click',  () => {
-    modal.classList.toggle('visible')
-})
+btnOpenModal.addEventListener("click", () => {
+  modal.classList.toggle("visible");
+});
 
-btnCloseModal.addEventListener('click', () => {
-    modal.classList.toggle('visible')
-})
+btnCloseModal.addEventListener("click", () => {
+  modal.classList.toggle("visible");
+});
 
+const formModal = document.querySelector("#form");
+const btn = document.querySelector("[data-js = btnSave]");
+const item = [];
 
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const title = document.querySelector("#title");
+  const date = document.querySelector("#date");
+  const description = document.querySelector("#description");
 
-const formModal = document.querySelector('#form')
-const btn = document.querySelector('[data-js = btnSave]')
-const item = []
+  let value = {
+    title: title.value,
+    date: date.value,
+    description: description.value,
+    like: false,
+  };
 
+  item.push(value);
 
-btn.addEventListener('click', (e) => {
-    e.preventDefault()
-    const title = document.querySelector("#title")
-    const date = document.querySelector('#date')
-    const description = document.querySelector('#description')
-    
+  // VERIFICAR
+  if (!localStorage) {
+    localStorage.setItem("codelandia", JSON.stringify(item));
+  } else {
+    localStorage.setItem("codelandia", JSON.stringify(item));
+  }
 
-    let value = {
-        title: title.value,
-        date: date.value,
-        description: description.value,
-        like: false
-    }
-
-    
-    const local = localStorage.getItem('codelandia')
-    
-    console.log(local)
-    item.push(value)
-
-    // VERIFICAR
-    if(!localStorage) {
-        localStorage.setItem('codelandia',JSON.stringify(item))
-    } else {
-        localStorage.setItem('codelandia', JSON.stringify(item))        
-    }
-
-})
-
+  modal.classList.remove("visible");
+});
 
 function getLocalStorage() {
-    const value = JSON.parse(localStorage.getItem('codelandia'))
-    if(!value ) {
-        console.log('nada consta')
-    } else {
+  const value = JSON.parse(localStorage.getItem("codelandia"));
+  const emptyImg = document.querySelector("[data-js=emptyImg]");
+  if (!value) {
+    emptyImg.classList.add("visible");
+  } else {
+    // remove o retorno quando nao tem valores
+    emptyImg.parentNode.removeChild(emptyImg);
 
-        value.forEach(item => {
-            
-            const dateSecond = Date.parse(item.date)
-            
-            
-            //formatando a data
-            const dateFormater = new Date(dateSecond).toLocaleDateString('pt-br', {
-                year: 'numeric',
-                month: 'short',
-                day: '2-digit'
-            })
-            
-            //fazendo uma copia do array
-            let array = item
-        //criando um novo array com a data formatada
-        array.date = dateFormater
+    value.forEach((item) => {
+      const dateSecond = Date.parse(item.date);
 
-        //exibindo as informações no html
-        const container = document.querySelector('[data-js=container]')
-        const html = `
+      //formatando a data
+      const dateFormater = new Date(dateSecond).toLocaleDateString("pt-br", {
+        year: "numeric",
+        month: "short",
+        day: "2-digit",
+      });
+
+      //fazendo uma copia do array
+      let array = item;
+      //criando um novo array com a data formatada
+      array.date = dateFormater;
+
+      //exibindo as informações no html
+      const container = document.querySelector("[data-js=container]");
+      const html = `
         <section class="c-card">
         <div class="c-card__header">
           <p class="c-card__date">${array.date}</p>
           <div class="c-card__icon">
           ${
-            array.like? '<img src="./assets/heart.svg" alt="Curtir" />' : '<img src="./assets/heart-outlined.svg" alt="Curtir" />'
+            array.like
+              ? '<img src="./assets/heart.svg" alt="Curtir" />'
+              : '<img src="./assets/heart-outlined.svg" alt="Curtir" />'
           }
         
         
@@ -92,12 +87,10 @@ function getLocalStorage() {
           </p>
           </div>
           </section>
-          `
-          container.innerHTML += html
-        }
-        );
-    }
+          `;
+      container.innerHTML += html;
+    });
+  }
 }
 
-
-getLocalStorage()
+getLocalStorage();
