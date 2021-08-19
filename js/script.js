38c08277100f90070d0578fd9eea037edea6ddb1
ghdpreto <ghdpreto@gmail.com>
2021-08-18 21:45:18 -0300

    refatorando codigo js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const btnOpenModal = document.querySelector("[data-js = btnOpenModal]");
 const btnCloseModal = document.querySelector("[data-js = btnCloseModal");
+const btnSave = document.querySelector("[data-js = btnSave]");
 const modal = document.querySelector("[data-js = modal]");
+const container = document.querySelector("[data-js=container]");
 
 btnOpenModal.addEventListener("click", () => {
   modal.classList.toggle("visible");
@@ -10,33 +12,25 @@ btnCloseModal.addEventListener("click", () => {
   modal.classList.toggle("visible");
 });
 
-const formModal = document.querySelector("#form");
-const btn = document.querySelector("[data-js = btnSave]");
-const item = [];
-
-btn.addEventListener("click", (e) => {
+btnSave.addEventListener("click", (e) => {
   e.preventDefault();
   const title = document.querySelector("#title");
   const date = document.querySelector("#date");
   const description = document.querySelector("#description");
 
-  let value = {
+  const value = {
     title: title.value,
     date: date.value,
     description: description.value,
     like: false,
   };
 
-  item.push(value);
+  setLocalStorage(value);
 
-  // VERIFICAR
-  if (!localStorage) {
-    localStorage.setItem("codelandia", JSON.stringify(item));
-  } else {
-    localStorage.setItem("codelandia", JSON.stringify(item));
-  }
+  // exibe/escodne o modal
+  modal.classList.toggle("visible");
 
-  modal.classList.remove("visible");
+  getLocalStorage();
 });
 
 function getLocalStorage() {
@@ -46,46 +40,73 @@ function getLocalStorage() {
   if (!value) {
     emptyImg.classList.add("visible");
   } else {
-    // remove o retorno quando nao tem valores
-    emptyImg.parentNode.removeChild(emptyImg);
+    if (emptyImg) {
+      // remove o retorno quando nao tem valores
+      emptyImg.parentNode.removeChild(emptyImg);
+    }
 
+    //remove os cards para gerar conforme a lista atualizada
+    container.innerHTML = "";
+
+    // desenhando os cards
     value.forEach((item) => {
       const formattedDate = formatDate(item.date);
 
-      //criando um novo array com a data formatada
+      //passando a data formatada para desenhar no card
       item.date = formattedDate;
 
-      //exibindo as informações no html
-      const container = document.querySelector("[data-js=container]");
-      const html = `
-        <section class="c-card">
-        <div class="c-card__header">
-          <p class="c-card__date">${item.date}</p>
-          <div class="c-card__icon">
-          ${
-            item.like
-              ? '<img src="./assets/heart.svg" alt="Curtir" />'
-              : '<img src="./assets/heart-outlined.svg" alt="Curtir" />'
-          }
-        
-        
-          </div>
-          </div>
-          
-          <div>
-          <h1 class="c-card__title">${item.title}</h1>
-          <p class="c-card__description">
-          ${item.description}
-          </p>
-          </div>
-          </section>
-          `;
-      container.innerHTML += html;
+      renderCard(item);
     });
   }
 }
 
+function setLocalStorage(item) {
+  let listItem = [];
+  if (localStorage.hasOwnProperty("codelandia")) {
+    listItem = JSON.parse(localStorage.getItem("codelandia"));
+  }
+
+  //cria um id
+  item.id = listItem.length + 1;
+  listItem.push(item);
+
+  //ordenando por data
+  listItem = listItem.sort(orderDate);
+  localStorage.setItem("codelandia", JSON.stringify(listItem));
+}
+
+function renderCard(item) {
+  //exibindo as informações no html
+  const html = `
+  <section class="c-card">
+  <div class="c-card__header">
+  <p class="c-card__date">${item.date}</p>
+  <div class="c-card__icon">
+  ${
+    item.like
+      ? '<img src="./assets/heart.svg" alt="Curtir" />'
+      : '<img src="./assets/heart-outlined.svg" alt="Curtir" />'
+  }
+        </div>
+        </div>
+      
+        <div>
+        <h1 class="c-card__title">${item.title}</h1>
+        <p class="c-card__description">
+        ${item.description}
+        </p>
+      </div>
+      </section>
+      `;
+  container.innerHTML += html;
+}
+
 getLocalStorage();
+
+//### FUNCOES EXTRAS ###
+function orderDate(a, b) {
+  return a.date < b.date;
+}
 
 function formatDate(date) {
   const value = new Date(date);
